d9317ff2c4150ebfda412c99ffe5eaefa98e845c
André Vasconcelos <andre.bruno@resourceit.com>
2019-12-26 17:39:19 -0300

    Criando primeira tela com springboot

--- src/main/java/br/com/caelum/listaVip/config/Configuracao.java
+++ src/main/java/br/com/caelum/listaVip/config/Configuracao.java
@@ -2,20 +2,9 @@ package br.com.caelum.listaVip.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
-@Controller
 public class Configuracao {
-	
-	@RequestMapping("/")
-	@ResponseBody
-	public String ola(){
-		return "<marquee>Olá, bem vindo ao sistema de lista vip! <blink><b>Topzeira!</b></blink></marquee>";
-	}
-	
 	public static void main(String[] args) {
 		SpringApplication.run(Configuracao.class, args);
 	}
